6030b2146e0f1b5e6d68a8b95382d1506aa2ce52
Arseniy Malyuk <arsenmaliuk12@gmail.com>
2025-03-26 15:38:33 +0100

    Cleaned up code

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,6 +1,11 @@
 import java.awt.Image;
+import java.util.Random;
 
 public abstract class Ghost extends Entity {
+    private String[] directions = {"LEFT", "RIGHT", "UP", "DOWN"};
+    private String newDirection = "";
+    private Random random = new Random();
+
     public Ghost(Image image, int x, int y, int width, int height) {
         super(image, x, y, width, height);
     }
