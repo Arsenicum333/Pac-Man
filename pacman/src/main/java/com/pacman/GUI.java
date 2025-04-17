e219de1af374aac494e757bd9fa642bfefb2a605
Arseniy Malyuk <arsenmaliuk12@gmail.com>
2025-04-17 11:59:44 +0200

    Add FontLoader class for handling fonts, Add arcade-style text labels

--- pacman/src/main/java/com/pacman/GUI.java
+++ pacman/src/main/java/com/pacman/GUI.java
@@ -2,6 +2,7 @@ package com.pacman;
 
 import com.pacman.helpers.GameObject;
 import com.pacman.helpers.ImageLoader;
+import com.pacman.helpers.FontLoader;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +10,7 @@ import java.awt.*;
 public class GUI extends JPanel {
     private static final GUI instance = new GUI();
     private Maze maze;
+    private Game game;
 
     private GUI() {
         maze = Maze.getInstance();
@@ -20,12 +22,12 @@ public class GUI extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
+        int windowWidth = getWidth();
+        int windowHeight = getHeight();
         int mazeWidth = Maze.getColumnCount() * Maze.getTileSize();
         int mazeHeight = Maze.getRowCount() * Maze.getTileSize();
-        int offsetX = (panelWidth - mazeWidth) / 2;
-        int offsetY = (panelHeight - mazeHeight) / 2;
+        int offsetX = (windowWidth - mazeWidth) / 2;
+        int offsetY = (windowHeight - mazeHeight) / 2;
 
         for (GameObject wall : maze.getWalls()) {
             g.drawImage(wall.getImage(), offsetX + wall.getX(), offsetY + wall.getY(),
@@ -63,31 +65,70 @@ public class GUI extends JPanel {
                     Maze.getTileSize(), Maze.getTileSize(), this);
 
         g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.setFont(FontLoader.getJoystixMonospaceFont(22f));
 
-        String scoreText = "Score: " + maze.getScore();
-        String highScoreText = "High Score: " + maze.getHighScore();
-        int scoreTextWidth = g.getFontMetrics().stringWidth(scoreText);
-        int highScoreTextWidth = g.getFontMetrics().stringWidth(highScoreText);
-        int centerX = panelWidth / 2;
-        int scoreX = centerX - scoreTextWidth / 2 - 180;
-        int highScoreX = centerX - highScoreTextWidth / 2 + 150;
-        int textY = 50;
+        String scoreLabel = "SCORE";
+        String scoreValue = String.valueOf(maze.getScore());
+        String highScoreLabel = "HIGH SCORE";
+        String highScoreValue = String.valueOf(maze.getHighScore());
+        FontMetrics metrics = g.getFontMetrics();
 
-        g.drawString(scoreText, scoreX, textY);
-        g.drawString(highScoreText, highScoreX, textY);
+        int scoreLabelWidth = metrics.stringWidth(scoreLabel);
+        int scoreValueWidth = metrics.stringWidth(scoreValue);
+        int highScoreLabelWidth = metrics.stringWidth(highScoreLabel);
+        int highScoreValueWidth = metrics.stringWidth(highScoreValue);
+        int centerX = windowWidth / 2;
+        int scoreX = centerX - scoreLabelWidth / 2 - 200;
+        int highScoreX = centerX - highScoreLabelWidth / 2 + 150;
+        int labelY = 30;
+        int valueY = labelY + metrics.getHeight();
 
-        int lives = maze.getPacman().getLives();
-        int lifeIconSize = 48;
-        int lifeIconSpacing = 10;
-        int livesY = offsetY + mazeHeight + 10;
-        int livesX = offsetX;
+        g.drawString(scoreLabel, scoreX, labelY);
+        g.drawString(scoreValue, scoreX + (scoreLabelWidth - scoreValueWidth) / 2, valueY);
+        g.drawString(highScoreLabel, highScoreX, labelY);
+        g.drawString(highScoreValue, highScoreX + (highScoreLabelWidth - highScoreValueWidth) / 2, valueY);
 
-        for (int i = 0; i < lives; i++) {
-            g.drawImage(ImageLoader.getImage("PacManIcon"), livesX + i * (lifeIconSize + lifeIconSpacing), livesY,
-                        lifeIconSize, lifeIconSize, this);
+        if (game != null) {
+            int lives = maze.getPacman().getLives();
+            int lifeIconSize = 48;
+            int lifeIconSpacing = 10;
+            int livesY = offsetY + mazeHeight + 10;
+            int livesX = offsetX;
+
+            for (int i = 0; i < lives; i++) {
+                g.drawImage(ImageLoader.getImage("PacManIcon"), livesX + i * (lifeIconSize + lifeIconSpacing), livesY,
+                            lifeIconSize, lifeIconSize, this);
+            }
+
+            if (game.isGameOver()) {
+                g.setFont(FontLoader.getJoystixMonospaceFont(60f));
+                g.setColor(Color.RED);
+
+                String gameOverText = "GAME OVER";
+                int gameOverWidth = g.getFontMetrics().stringWidth(gameOverText);
+
+                g.drawString(gameOverText, centerX - gameOverWidth / 2, windowHeight / 2);
+
+                g.setFont(FontLoader.getJoystixMonospaceFont(22f));
+                g.setColor(Color.WHITE);
+
+                String restartText = "PRESS SPACE TO RESTART";
+                int restartWidth = g.getFontMetrics().stringWidth(restartText);
+
+                g.drawString(restartText, centerX - restartWidth / 2, windowHeight / 2 + 50);
+            } else if (!game.isGameOn()) {
+                g.setFont(FontLoader.getJoystixMonospaceFont(22f));
+                g.setColor(Color.YELLOW);
+
+                String startText = "PRESS ARROW KEY OR WASD TO START";
+                int startWidth = g.getFontMetrics().stringWidth(startText);
+
+                g.drawString(startText, centerX - startWidth / 2, windowHeight / 2);
+            }
         }
     }
 
     public static GUI getInstance() {return instance;}
+
+    public void setGame(Game game) {this.game = game;}
 }
---
+++ pacman/src/main/java/com/pacman/helpers/FontLoader.java
@@ -0,0 +1,37 @@
+package com.pacman.helpers;
+
+import java.awt.*;
+import java.io.InputStream;
+
+public class FontLoader {
+    private static Font pressStart2PFont;
+    private static Font arcadeClassicFont;
+    private static Font joystixMonospaceFont;
+
+    static {
+        pressStart2PFont = loadFont("fonts/PressStart2P.ttf");
+        arcadeClassicFont = loadFont("fonts/ArcadeClassic.ttf");
+        joystixMonospaceFont = loadFont("fonts/JoystixMonospace.otf");
+    }
+
+    private static Font loadFont(String path) {
+        try {
+            InputStream is = FontLoader.class.getClassLoader().getResourceAsStream(path);
+
+            if (is == null) {
+                System.err.println("Could not find font: " + path);
+                return new Font("Arial", Font.PLAIN, 12);
+            }
+
+            return Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (Exception e) {
+            System.err.println("Failed to load font: " + path);
+            e.printStackTrace();
+            return new Font("Arial", Font.PLAIN, 12);
+        }
+    }
+
+    public static Font getPressStartFont(float size) {return pressStart2PFont.deriveFont(size);}
+    public static Font getArcadeClassicFont(float size) {return arcadeClassicFont.deriveFont(size);}
+    public static Font getJoystixMonospaceFont(float size) {return joystixMonospaceFont.deriveFont(size);}
+}
