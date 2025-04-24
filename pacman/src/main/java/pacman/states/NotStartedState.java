29a520ad613d9c7f3ce7f895a192d63bc43bcf9e
Arseniy Malyuk <arsenmaliuk12@gmail.com>
2025-04-24 19:39:58 +0200

    Add logging for NotStartedState

--- pacman/src/main/java/pacman/states/NotStartedState.java
+++ pacman/src/main/java/pacman/states/NotStartedState.java
@@ -2,11 +2,26 @@ package pacman.states;
 
 import pacman.Game;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NotStartedState implements GameState {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotStartedState.class);
+
     @Override
     public void handleGameLoop(Game game) {}
+
     @Override
-    public void startGame(Game game) {game.setState(new PlayingState());}
+    public void startGame(Game game) {
+        try {
+            game.setState(new PlayingState());
+            LOGGER.info("Game started from NotStarted state");
+        } catch (Exception e) {
+            LOGGER.error("Failed to start game from NotStartedState", e);
+            throw new RuntimeException("Game start failed", e);
+        }
+    }
+
     @Override
     public void restartGame(Game game) {}
     @Override
